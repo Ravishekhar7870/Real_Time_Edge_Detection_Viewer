c07fa49ce55f037938f69c4a4de9df7de61429c2
Ravishekhar Yadav <yadavravishekhar01@gmail.com>
2025-06-11 00:09:30 +0530

    added code to gain user camera acess when the app is build in the onCreate function of the mainactivity file

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.myapplication;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.util.Log;
 import android.view.TextureView;
 
 
@@ -16,8 +21,11 @@ import android.view.TextureView;
 
 
 public class MainActivity extends AppCompatActivity {
-
-
+    private void startCamera() {
+        // You will later put your CameraX or Camera2 setup here
+        Log.d("CAMERA", "Camera permission granted. Start camera preview...");
+    }
+    private static final int CAMERA_PERMISSION_REQUEST_CODE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +37,16 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-          // Blue for processed view
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission not granted, request it
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA},
+                    CAMERA_PERMISSION_REQUEST_CODE);
+        } else {
+            // Permission already granted
+            startCamera();
+        }
 
 
     }
