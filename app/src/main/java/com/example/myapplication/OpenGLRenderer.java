27699641b8a912c28caca41ca4bdd7e3377407ba
Ravishekhar Yadav <yadavravishekhar01@gmail.com>
2025-06-11 14:57:12 +0530

    update code of OpenGLRender.java to solve the issue of not displaying processed data

--- app/src/main/java/com/example/myapplication/OpenGLRenderer.java
+++ app/src/main/java/com/example/myapplication/OpenGLRenderer.java
@@ -12,27 +12,23 @@ import javax.microedition.khronos.opengles.GL10;
 
 public class OpenGLRenderer implements GLSurfaceView.Renderer {
 
+    private int textureId;
     private ByteBuffer imageData;
-    private int textureId = -1;
-    private int width, height;
     private boolean dataReady = false;
+    private int width = 640, height = 480;
 
-    // Fullscreen quad
-    private final float[] vertices = {
-            -1f, 1f, 0f, 1f,
-            -1f, -1f, 0f, 0f,
-            1f, 1f, 1f, 1f,
-            1f, -1f, 1f, 0f
-    };
+    private int program;
+    private int aPosition, aTexCoord, uTexture;
+
     private FloatBuffer vertexBuffer;
 
-    public OpenGLRenderer() {
-        vertexBuffer = ByteBuffer.allocateDirect(vertices.length * 4)
-                .order(ByteOrder.nativeOrder())
-                .asFloatBuffer();
-        vertexBuffer.put(vertices);
-        vertexBuffer.position(0);
-    }
+    private final float[] vertices = {
+            // X, Y,    U, V
+            -1f, 1f,   0f, 0f,
+            -1f, -1f,  0f, 1f,
+            1f, 1f,    1f, 0f,
+            1f, -1f,   1f, 1f,
+    };
 
     public void updateImage(byte[] data, int w, int h) {
         this.width = w;
@@ -45,13 +41,15 @@ public class OpenGLRenderer implements GLSurfaceView.Renderer {
 
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-        textureId = createTexture();
         GLES20.glClearColor(0f, 0f, 0f, 1f);
+        textureId = createTexture();
+        setupShaders();
+        setupVertexBuffer();
     }
 
     @Override
-    public void onSurfaceChanged(GL10 gl, int width, int height) {
-        GLES20.glViewport(0, 0, width, height);
+    public void onSurfaceChanged(GL10 gl, int w, int h) {
+        GLES20.glViewport(0, 0, w, h);
     }
 
     @Override
@@ -74,7 +72,22 @@ public class OpenGLRenderer implements GLSurfaceView.Renderer {
             dataReady = false;
         }
 
-        // You would normally draw the quad here using shaders (not implemented yet)
+        GLES20.glUseProgram(program);
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureId);
+
+        GLES20.glEnableVertexAttribArray(aPosition);
+        GLES20.glEnableVertexAttribArray(aTexCoord);
+
+        vertexBuffer.position(0);
+        GLES20.glVertexAttribPointer(aPosition, 2, GLES20.GL_FLOAT, false, 4 * 4, vertexBuffer);
+
+        vertexBuffer.position(2);
+        GLES20.glVertexAttribPointer(aTexCoord, 2, GLES20.GL_FLOAT, false, 4 * 4, vertexBuffer);
+
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4);
+
+        GLES20.glDisableVertexAttribArray(aPosition);
+        GLES20.glDisableVertexAttribArray(aTexCoord);
     }
 
     private int createTexture() {
@@ -85,5 +98,52 @@ public class OpenGLRenderer implements GLSurfaceView.Renderer {
         GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
         GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
         return tex;
+    }
+
+    private void setupVertexBuffer() {
+        vertexBuffer = ByteBuffer.allocateDirect(vertices.length * 4)
+                .order(ByteOrder.nativeOrder())
+                .asFloatBuffer();
+        vertexBuffer.put(vertices);
+        vertexBuffer.position(0);
+    }
+
+    private void setupShaders() {
+        String vertexShaderCode =
+                "attribute vec4 a_Position;" +
+                        "attribute vec2 a_TexCoord;" +
+                        "varying vec2 v_TexCoord;" +
+                        "void main() {" +
+                        "  gl_Position = a_Position;" +
+                        "  v_TexCoord = a_TexCoord;" +
+                        "}";
+
+        String fragmentShaderCode =
+                "precision mediump float;" +
+                        "uniform sampler2D u_Texture;" +
+                        "varying vec2 v_TexCoord;" +
+                        "void main() {" +
+                        "  float g = texture2D(u_Texture, v_TexCoord).r;" +
+                        "  gl_FragColor = vec4(g, g, g, 1.0);" +
+                        "}";
+
+        int vertexShader = compileShader(GLES20.GL_VERTEX_SHADER, vertexShaderCode);
+        int fragmentShader = compileShader(GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode);
+
+        program = GLES20.glCreateProgram();
+        GLES20.glAttachShader(program, vertexShader);
+        GLES20.glAttachShader(program, fragmentShader);
+        GLES20.glLinkProgram(program);
+
+        aPosition = GLES20.glGetAttribLocation(program, "a_Position");
+        aTexCoord = GLES20.glGetAttribLocation(program, "a_TexCoord");
+        uTexture = GLES20.glGetUniformLocation(program, "u_Texture");
+    }
+
+    private int compileShader(int type, String code) {
+        int shader = GLES20.glCreateShader(type);
+        GLES20.glShaderSource(shader, code);
+        GLES20.glCompileShader(shader);
+        return shader;
     }
 }
